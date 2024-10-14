41e759cbb3cb0985571a6b581436d3cc5cb61b3e
Johnanthon <delacruzjohnanthon@gmail.com>
2024-10-14 18:29:48 +0800

    test

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
 
 // Adjust the path to 'initialize.php' correctly
