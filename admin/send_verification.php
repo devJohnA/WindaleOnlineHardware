6b9e96a617f459028fec88659bee5c7f8280eaf5
Johnanthon <delacruzjohnanthon@gmail.com>
2024-11-16 22:54:35 +0800

    se

--- admin/send_verification.php
+++ admin/send_verification.php
@@ -55,7 +55,7 @@ try {
 
     // Create verification link
     $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
-    $verification_link = $actual_link . "/windalehardware.com/admin/verify.php?token=" . $token;
+    $verification_link = $actual_link . "/admin/verify.php?token=" . $token;
 
     // Create email HTML content
     $emailBody = "
