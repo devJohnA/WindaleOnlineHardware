d7647d0713bbb15d50adf2ca3e01bae8e7c6f8f4
Johnanthon <delacruzjohnanthon@gmail.com>
2024-11-16 23:15:08 +0800

    add

---
+++ addcolumn.php
@@ -0,0 +1,29 @@
+<?php
+// Database connection details
+$servername = "localhost";
+$username = "u510162695_dried"; 
+$password = "1Dried_password"; 
+$dbname = "u510162695_dried"; 
+// Create a new MySQLi connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check if the connection was successful
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+$sql = "ALTER TABLE `tbluseraccount`
+    ADD COLUMN `OTP` varchar(6) DEFAULT NULL, 
+    ADD COLUMN `OTP_TIMESTAMP` timestamp NULL DEFAULT NULL;";
+
+// Execute the query
+if ($conn->query($sql) === TRUE) {
+    echo "Columns added successfully to tbluseraccount";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+// Close the connection
+$conn->close();
+?>
