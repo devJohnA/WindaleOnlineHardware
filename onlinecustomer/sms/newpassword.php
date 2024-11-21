aea1f53c91dbe9b64f74b3b57ef42318d7f0f445
Johnanthon <delacruzjohnanthon@gmail.com>
2024-11-21 15:08:18 +0800

    icon

--- onlinecustomer/sms/newpassword.php
+++ onlinecustomer/sms/newpassword.php
@@ -60,6 +60,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../../img/windales.png">
     <title>Reset Password</title>
     <style>
         body {
--- onlinecustomer/sms/send-otp.php
+++ onlinecustomer/sms/send-otp.php
@@ -122,6 +122,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../../img/windales.png">
     <title>OTP Verification</title>
     <style>
         body {
