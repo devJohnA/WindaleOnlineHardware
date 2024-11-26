0ba73ca2318cdbd060822e5bc3b285cd96e5fd2f
DanteMontecalvo <dantemontecalvo04@gmail.com>
2024-11-26 19:18:11 +0800

    Update title

--- onlinecustomer/index.php
+++ onlinecustomer/index.php
@@ -27,7 +27,7 @@ $recaptcha_site_key = '6Lcjy34qAAAAAD0k2NNynCgcbE6_W5Fy9GotDBZA';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Page</title>
+    <title>Login Form</title>
     <link rel="icon" href="../img/windales.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
