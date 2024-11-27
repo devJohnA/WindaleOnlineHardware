0a73a75701f2f139429beab098fce86c74602ad2
Johnanthon <delacruzjohnanthon@gmail.com>
2024-11-28 01:59:57 +0800

    ddd

--- backend/get_customers.php
+++ backend/get_customers.php
@@ -1,6 +1,8 @@
 <?php
 header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
 header('Access-Control-Allow-Headers: Content-Type');
+
 header('Content-Type: application/json');
 $servername = "localhost";
 $username = "u510162695_dried"; 
