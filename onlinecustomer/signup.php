144b3664afda0a19e7237f8a21c8172e91cf4b58
Johnanthon <delacruzjohnanthon@gmail.com>
2024-11-28 21:30:26 +0800

    dd

--- onlinecustomer/signup.php
+++ onlinecustomer/signup.php
@@ -125,6 +125,7 @@ ini_set('display_errors', 1);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
     <style>
         body {
             background: #f5f5f5;
