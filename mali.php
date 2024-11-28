cb663feb7dce4f3d1b4a83277b71e6d04abd5ec8
Johnanthon <delacruzjohnanthon@gmail.com>
2024-11-29 01:34:04 +0800

    sdsdsdsd

--- mali.php
+++ mali.php
@@ -1,9 +1,11 @@
 <?php
-$image_directory = 'customer/customer_image/';
+$image_directory = $root_path . '/customer/customer_image/';
 $files = glob($image_directory . "*.{jpg,jpeg,png,gif}", GLOB_BRACE);
 
-foreach($files as $file): ?>
-    <img src="<?php echo htmlspecialchars($file); ?>" 
+foreach($files as $file): 
+    $web_path = '/customer/customer_image/' . basename($file);
+?>
+    <img src="<?php echo htmlspecialchars($web_path); ?>" 
          alt="Profile Image" 
          class="profile-image"
          onerror="console.log('Failed to load: ' + this.src);">
