d42a3a9943373ae0dfb74c4070be81245184960d
Johnanthon <delacruzjohnanthon@gmail.com>
2024-11-29 01:31:05 +0800

    ddds

--- mali.php
+++ mali.php
@@ -1,17 +1,10 @@
 <?php
-$image_directory = $_SERVER['DOCUMENT_ROOT'] . '/customer/customer_image/';
-$web_path = '/customer/customer_image/';  // Web-accessible path
+$image_directory = 'customer/customer_image/';
+$files = glob($image_directory . "*.{jpg,jpeg,png,gif}", GLOB_BRACE);
 
-$images = array_filter(scandir($image_directory), function($file) use ($allowed_extensions) {
-    $ext = strtolower(pathinfo($file, PATHINFO_EXTENSION));
-    return $file != '.' && $file != '..' && in_array($ext, $allowed_extensions);
-});
-?>
-
-<div class="image-grid">
-    <?php foreach ($images as $image): ?>
-        <img src="<?php echo htmlspecialchars($web_path . $image); ?>"
-             alt="<?php echo htmlspecialchars($image); ?>"
-             class="profile-image">
-    <?php endforeach; ?>
-</div>
+foreach($files as $file): ?>
+    <img src="<?php echo htmlspecialchars($file); ?>" 
+         alt="Profile Image" 
+         class="profile-image"
+         onerror="console.log('Failed to load: ' + this.src);">
+<?php endforeach; ?>
