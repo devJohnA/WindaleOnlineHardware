feaf5cca420fb7b4254ced3763f3c8813681119b
Johnanthon <delacruzjohnanthon@gmail.com>
2024-12-11 22:43:18 +0800

    new changes

--- admin/process_recovery.php
+++ admin/process_recovery.php
@@ -81,7 +81,7 @@ try {
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
     $mail->Username = 'delacruzjohnanthon@gmail.com';
-    $mail->Password = 'xaktlaqhjsgksqhi'; 
+    $mail->Password = 'mdbdvmnypposvdtk'; 
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
     $mail->Port = 587;
 
